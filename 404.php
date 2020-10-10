887afa51cbbb04d09ef41e37f2386c42556b06d1
marketinstance <71776991+marketinstance@users.noreply.github.com>
2020-10-10 20:52:58 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>PHP веб сайт</title>
+</head>
+<body>
+	
+    <?php 
+        require "blocks/header.php"
+    ?>
+
+    <div class="container mt-5">
+
+        <h3>Ошибка 404</h3><br>
+        <h3>Страница не найдена</h3>
+
+    </div>
+
+  <?php 
+      require "blocks/footer.php"
+  ?>
+  
+</body>
+</html>
